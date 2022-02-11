3f107dae33146b47ad90c6597a722ed9d5b36425
Susanne <susanne.frick@uni-mannheim.de>
2022-02-11 17:31:01 +0100

    corrected some mistakes

--- R/calc_info_pairs.R
+++ R/calc_info_pairs.R
@@ -1,21 +1,21 @@
 calc.info.pairs <- function(traits, int, loads, uni, design.mat) {
   
-  npairs <- K*choose(nb,2)
+  npairs <- nrow(design.mat)
   
-  comp.mat.a <- design.mat %*% load.mat
+  comp.mat.a <- design.mat %*% loads
   comp.uni <- design.mat^2 %*% uni
-  comp.load <- apply(comp.mat.a, 1, function(rw) tcrossprod(rw, rw), simplify=FALSE)
+  comp.load <- lapply(1:nrow(comp.mat.a), function(rind, ca) tcrossprod(ca[rind,], ca[rind,]), ca=comp.mat.a)
   
   all.infos <- vector("list", nrow(traits))
-  for (n in 1:length(all.infos)) all.infos[[n]] <- array(dim=c(K,ncol(loads),ncol(loads)))
+  for (n in 1:length(all.infos)) all.infos[[n]] <- array(dim=c(npairs, ncol(loads), ncol(loads)))
   
   for (n in 1:nrow(traits)) {
-    inner.tirt <- (comp.mat.a%*%traits[n,] + as.vector(gamma.true))/sqrt(as.vector(design.mat^2 %*% uni))
+    inner.tirt <- (comp.mat.a%*%traits[n,] + as.vector(int))/sqrt(as.vector(comp.uni))
     inner.info <- dnorm(inner.tirt)^2/(pnorm(inner.tirt)*(1-pnorm(inner.tirt)))
     
     info.pairs <- lapply(1:npairs, function(ind, cuni, cload, iinfo) 1/comp.uni[ind] * comp.load[[ind]] * inner.info[ind],
                          cuni=comp.uni, cload=comp.load, iinfo=inner.info)
-    all.infos[[n]] <- array(t(matrix(unlist(info.pairs), ncol(load.mat)^2, npairs)), dim=c(npairs, ncol(load.mat), ncol(load.mat)))
+    all.infos[[n]] <- array(t(matrix(unlist(info.pairs), ncol(loads)^2, npairs)), dim=c(npairs, ncol(loads), ncol(loads)))
   }
   
   return(all.infos)
