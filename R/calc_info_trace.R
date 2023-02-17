e31358727f28b540a48f935a46c85b94d1d89924
susanne-frick <frick@statistik.tu-dortmund.de>
2023-02-17 10:15:21 +0100

    added argument prior

--- R/calc_info_trace.R
+++ R/calc_info_trace.R
@@ -4,6 +4,7 @@
 #'
 #' @param infos block information as returned from calc.info.block()
 #' @param avg logical, average result across persons? defaults to FALSE
+#' @param prior prior covariance matrix for the traits, defaults to NULL
 #'
 #' @return matrix of traces of information matrices, rows=persons, columns=blocks
 #' @export
@@ -12,7 +13,11 @@
 #' It can be used to summarize information for a block in one number, weighting all precision for all traits equally.
 #' It is inversely proportional to standard errors.
 #'
-calc.info.trace <- function(infos, avg=F) {
+calc.info.trace <- function(infos, avg=F, prior = NULL) {
+  if(isFALSE(is.null(prior))) {
+    prior <- array(rep(prior, each = dim(infos[[1]])[1]), dim = dim(infos[[1]]))
+    infos <- lapply(infos, function(ip, p) ip + p, p = prior)
+  }
   info.trace <- do.call(rbind, lapply(infos, function(ip) apply(ip, 1, function(i) sum(diag(i)))))
   if(avg==TRUE) {
     return(colSums(info.trace))
