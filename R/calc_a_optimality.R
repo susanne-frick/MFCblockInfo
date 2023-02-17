170fabef1d6cd5885e99949e16289bf70f3cdc3d
susanne-frick <frick@statistik.tu-dortmund.de>
2023-02-17 10:12:58 +0100

    added arguments prior and summed (allows to calculate for each block separately)

--- R/calc_a_optimality.R
+++ R/calc_a_optimality.R
@@ -1,11 +1,20 @@
 #' calculate A-optimality
 #'
 #' @param infos block information as returned from calc.block.infos
+#' @param summed logical, should the information be summed across blocks? defaults to TRUE
+#' @param prior matrix of prior covariances, if given, posterior information is computed, defaults to NULL
 #'
 #' @return numeric vector of A-optimality for each person
 #' @export
 #'
 #' @examples
-calc.a.optimality <- function(infos) {
-  rowSums(info2se(infos, var.out=T), na.rm=T)
+calc.a.optimality <- function(infos, prior=NULL, summed=TRUE) {
+  ses <- info2se(infos, var.out=T, prior=prior, summed=summed)
+  if(length(dim(ses)) == 3) {
+    rowSums(ses, dims = 2)    
+  } else if (length(dim(ses)) == 1){
+    return(ses) 
+  } else {
+    rowSums(ses, na.rm=T)
+  }
 }
--- R/calc_d_optimality.R
+++ R/calc_d_optimality.R
@@ -1,11 +1,30 @@
 #' Calculate D-optimality
 #'
 #' @param infos block information as returned from calc.info.block()
+#' @param summed logical, should the information be summed across blocks? defaults to TRUE
+#' @param prior matrix of prior covariances, if given, posterior information is computed, defaults to NULL
 #'
 #' @return numeric vector of D-optimality for each person
 #' @export
 #'
 #' @examples
-calc.d.optimality <- function(infos) {
-  do.call(c, lapply(infos, function(ip) det(colSums(ip, dims=1))))
+calc.d.optimality <- function(infos, prior = NULL, summed = TRUE) {
+  # sum across blocks if there are several
+  if(isTRUE(summed) & (length(dim(infos[[1]])) > 2)) {
+    infos <- lapply(infos, function(ip) matrix(colSums(ip, dims=1), dim(ip)[2], dim(ip)[3]))
+  }
+  
+  if(isFALSE(is.null(prior))) {
+    # expand the prior if there are several blocks (or not summed)
+    if(length(dim(infos[[1]])) > 2) prior <- array(rep(prior, each = dim(infos[[1]])[1]), dim = dim(infos[[1]]))
+    # add the prior
+    infos <- lapply(infos, function(ip, p) ip + p, p = prior)
+  }
+  #calculate determinant
+  if(length(dim(infos[[1]])) > 2) {
+    do.call(rbind, lapply(infos, function(ip) apply(ip, 1, det)))
+  } else {
+    do.call(c, lapply(infos, function(ip) det(ip)))
+  }
+  
 }
